8f10351e7ce925efb5fed71fcde5219ebfb7f5c1
gazalinawaz <gazalinawaz@gmail.com>
2020-09-10 19:30:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,3 @@
-data "aws_subnet_ids" "private" {
-  vpc_id = vpc-04fa9b33dcff751dd
-
-  tags = {
-    Tier = "Private"
-  }
-}
-
 resource "aws_instance" "myec2" {
    ami = "ami-0c94855ba95c71c99"
    instance_type = "t2.micro"
