04935f02437c93d2368954c670a25b5e766b82c1
gazalinawaz <gazalinawaz@gmail.com>
2020-09-11 21:52:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_iam_user" "lb" {
 resource "aws_instance" "myec2" {
   ami = "ami-0c94855ba95c71c99"
   instance_type = "t1.micro"
-  vpc_security_group_ids = ["sg-0f2f7164", "sg-0bd92f0cf4e31e9c9"]
+  vpc_security_group_ids = ["sg-0bdd6e22ed443882d", "sg-0bd92f0cf4e31e9c9"]
   key_name = "1"
   subnet_id = "subnet-014044f13548abb3c"
 }
