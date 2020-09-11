cd8e2eac5dd40ef976e401d489b2d92e12e7d88f
gazalinawaz <gazalinawaz@gmail.com>
2020-09-11 21:46:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,16 @@ resource "aws_iam_user" "lb" {
   name = "remoteuser"
   path = "/system/"
 }
+
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0c94855ba95c71c99"
+  instance_type = "t1.micro"
+  vpc_security_group_ids = ["sg-0f2f7164", "sg-0bd92f0cf4e31e9c9"]
+  key_name = "1"
+  subnet_id = "subnet-014044f13548abb3c"
+ 
+  tags {
+    Name = "manual"
+  }
+}
